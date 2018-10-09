9bda58a95437ced018ec24dcd3bc95ee1237ea2e
dellMax <s3496904@student.rmit.edu.au>
2018-10-09 21:46:24 +1100

    node construction

--- max/category_tree.py
+++ max/category_tree.py
@@ -2,23 +2,27 @@ from __future__ import print_function
 
 class Node(object):
     """Tree node: left and right child + data which can be any object"""
-    entityNumber = 50
-    producerNumber = 15
-    category = 1
 
     def __init__(self, user_id):
-        #get the user history by userId
-        history = [[1,(1,2,3),1],[1,(4,2,3),0],[1,(4,2),2],[3,(1,5,3),1]]
-        p = 0.8
-        producer_freq = [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        entity_freq = [0,1,2,2,0,0,1,2,2,0,0,1,2,2,0,0,1,2,2,0,0,1,2,2,0,0,1,2,2,0,0,1,2,2,0,0,1,2,2,0,0,1,2,2,0,0,1,2,2,0,]
-        long = [p,producer_freq,entity_freq]
-        short = [1,1,0,1,1]
-        self.user_id = user_id
-        self.p = p
-        self.long = long
-        self.short = short
-        self.history = history
+        with open("block_cate_user.txt") as f:
+            content = f.readlines()
+            for j in range(0,len(content)):
+                line = content[j].strip()
+                id = int(line.split(",")[0])
+                if user_id == id:
+                    p = line.split(",")[1]
+                    producer_freq = line.split(",[")[1].split("]")[0]
+                    entity_freq = line.split("],[")[1].split("]")[0]
+                    long = [p, producer_freq, entity_freq]
+                    history = self.user_history(id)
+                    short = []
+                    for s in range(0,5):
+                        short.append(int(history[s].split(":")[1].split(",")[0]))
+                    self.user_id = user_id
+                    self.p = p
+                    self.long = long
+                    self.short = short
+                    self.history = history
 
     def print_node(self):
         print("summary of user ",self.user_id)
@@ -28,9 +32,20 @@ class Node(object):
         print("short term window: ",self.short)
         print()
 
+    def user_history(self, user_id):
+        result = []
+        with open("user_hist.txt") as f:
+            content = f.readlines()
+            for i in range(0,len(content)):
+                if content[i].startswith(str(user_id)+":"):
+                    result.append(content[i].strip())
+        return result
 
-user_1 = Node(1)
-user_2 = Node(2)
-user_1.print_node()
-user_2.print_node()
+
+with open("block_cate_user.txt") as f:
+    content = f.readlines()
+    for j in range(0, len(content)):
+        user_id = int(content[j].split(",")[0])
+        user_node = Node(user_id)
+        user_node.print_node()
 
